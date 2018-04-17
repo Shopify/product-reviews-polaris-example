e257e268d2bcaf7b2e1e8560755fd8bc9e5b6158
Dominic McPhee <dominic.mcphee@gmail.com>
2018-04-17 17:19:38 -0400

    Formatting changes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Hello world</h1>
+        <p>Hello world</p>
       </div>
     );
   }
