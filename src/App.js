fe663c4cd802947be781e61a60d9d4d31fc9521c
Dominic McPhee <dominic.mcphee@gmail.com>
2018-04-17 23:19:14 -0400

    Adding server and more config

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React, {Component} from 'react';
+import ApolloClient from 'apollo-boost';
+import {ApolloProvider} from 'react-apollo';
+
+const client = new ApolloClient();
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <p>Hello world</p>
-      </div>
+      <ApolloProvider client={client}>
+        <div className="App">
+          <p>Hello world</p>
+        </div>
+      </ApolloProvider>
     );
   }
 }
---
+++ src/server.js
@@ -0,0 +1,98 @@
+import koa from 'koa';
+import koaRouter from 'koa-router';
+import koaBody from 'koa-bodyparser';
+import { graphqlKoa, graphiqlKoa } from 'apollo-server-koa';
+import { makeExecutableSchema } from 'graphql-tools';
+
+const app = new koa();
+const router = new koaRouter();
+const PORT = 3001;
+
+const typeDefs = `
+  type Product {
+    name: String
+    averageRating: Float
+    reviewCount: Int
+  }
+
+  type Customer {
+    name: String
+    email: String
+  }
+
+  type Review {
+    id: Int!
+    title: String
+    content: String
+    customer: Customer
+    product: Product
+    status: String
+    date: String
+  }
+
+  type Query {
+    reviews: [Review]
+    review(id: Int!): Review
+  }
+`;
+
+const reviews = [
+  {
+    id: 341,
+    url: "/reviews/341",
+    title: "Seriously amazing!",
+    content:
+      "No joke, the best hair pomade I have ever used. My hair typically gets a little too wavy and unruly for the hairstyle I have been going with, but the Pomade Paste totally gave me control over my hair. The hold is amazing, plus it keeps a great 'dry-look' to my hair which I like. And lastly, the pomade smells bomb. Seriously it's the best thing you could do for your hair.",
+    rating: 5,
+    customer: {
+      name: "Mae Jemison",
+      email: "mae.jemison@gmail.com"
+    },
+    product: {
+      name: "Pomade Paste",
+      averageRating: 4.4,
+      reviewCount: 6
+    },
+    status: "published",
+    date: "Mar 18, 6: 21pm"
+  },
+  {
+    id: 256,
+    url: "/reviews/256",
+    title: "Very nice with a lovely smell",
+    content:
+      "The Pomade Cream smells nice and leaves the hair with a nice finish. I have also been using it on my beard and it is doing a great job of keeping it tamed! Strongly recommend the product (N1) and I am looking forward to see what other products Craigmont is going to create.",
+    rating: 4,
+    customer: {
+      name: "Ellen Ochoa",
+      email: "ellen.ochoa@gmail.com"
+    },
+    product: {
+      name: "Pomade Cream",
+      averageRating: 3.6,
+      reviewCount: 4
+    },
+    status: "unpublished",
+    date: "Mar 24, 12: 13pm"
+  }
+];
+
+const resolvers = {
+  Query: {
+    reviews: () => reviews,
+  },
+};
+
+export const schema = makeExecutableSchema({
+  typeDefs,
+  resolvers
+});
+
+router.post('/graphql', koaBody(), graphqlKoa({ schema }));
+router.get('/graphql', graphqlKoa({ schema }));
+
+router.get('/graphiql', graphiqlKoa({ endpointURL: '/graphql' }));
+
+app.use(router.routes());
+app.use(router.allowedMethods());
+app.listen(PORT);
