737d4c78d2a0520c4f9b78587f3f8f72fdccb6a0
Dominic McPhee <dominic.mcphee@gmail.com>
2018-05-01 16:21:43 -0400

    Step 3

--- src/routes/ReviewList.js
+++ src/routes/ReviewList.js
@@ -1,6 +1,18 @@
 import React from 'react';
 import {graphql} from 'react-apollo';
 import gql from 'graphql-tag';
+import {
+  EmptyState,
+  Page,
+  Card,
+  TextContainer,
+  SkeletonBodyText,
+  SkeletonDisplayText,
+  ResourceList,
+} from '@shopify/polaris';
+
+import {settings} from '../icons';
+import ReviewListItem from '../components/ReviewListItem';
 
 function ReviewList(props) {
   const {
@@ -8,10 +20,57 @@ function ReviewList(props) {
   } = props;
 
   if (loading) {
-    return <div>Loading...</div>;
+    return (
+      <Page
+        title="Product reviews"
+        secondaryActions={[
+          {icon: settings, content: 'Settings', url: '/settings'},
+        ]}
+      >
+        <Card sectioned>
+          <TextContainer>
+            <SkeletonDisplayText size="small" />
+            <SkeletonBodyText />
+          </TextContainer>
+        </Card>
+      </Page>
+    );
   }
 
-  return <div>{reviews.length} reviews</div>;
+  const pageContent =
+    reviews.length === 0 ? (
+      <EmptyState
+        heading="You haven't received any reviews yet"
+        action={{content: 'Configure settings'}}
+        secondaryAction={{
+          content: 'Learn more',
+          url: 'https://help.shopify.com',
+        }}
+        image="/review-empty-state.svg"
+      >
+        <p>Once you have received reviews they will display on this page.</p>
+      </EmptyState>
+    ) : (
+      <Card>
+        <ResourceList
+          showHeader
+          resourceName={{singular: 'review', plural: 'reviews'}}
+          items={reviews}
+          renderItem={(review) => <ReviewListItem {...review} />}
+        />
+      </Card>
+    );
+
+  return (
+    <Page
+      title="Product reviews"
+      secondaryActions={[
+        {icon: settings, content: 'Settings', url: '/settings'},
+      ]}
+    >
+      {pageContent}
+    </Page>
+  );
 }
 
 export default graphql(gql`
